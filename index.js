31d76a3ca2bf02a55896de498f0181256c857e1d
Shakil khatri <khatri.shakilamd@gmail.com>
2023-10-20 18:23:53 +0530

    handled no params

--- index.js
+++ index.js
@@ -7,26 +7,28 @@ const port = 8080;
 
 app.get("/", async (req, res) => {
   const { url, selector } = req.query;
+  if (!url || !selector) res.send("Welcome to the server");
+  else {
+    try {
+      axios
+        .get(url)
+        .then((response) => {
+          const $ = cheerio.load(response.data);
+          const info = [];
 
-  try {
-    axios
-      .get(url)
-      .then((response) => {
-        const $ = cheerio.load(response.data);
-        const info = [];
-
-        $(selector).each((index, element) => {
-          info.push($(element).text());
+          $(selector).each((index, element) => {
+            info.push($(element).text());
+          });
+          console.log(info);
+          res.send(info);
+        })
+        .catch((error) => {
+          console.error(`Error occurred: ${error}`);
         });
-        console.log(info);
-        res.send(info);
-      })
-      .catch((error) => {
-        console.error(`Error occurred: ${error}`);
-      });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("An error occurred");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    }
   }
 });
 
