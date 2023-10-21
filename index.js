58aa29d558a92636dd9bff4acf3c8bd5133ab822
Shakil khatri <khatri.shakilamd@gmail.com>
2023-10-21 13:03:04 +0530

    scrape with request body

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import axios from "axios";
 import cheerio from "cheerio";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
@@ -18,6 +21,39 @@ app.get("/test", async (req, res) => {
 app.get("/scrape", async (req, res) => {
   const { url, selector } = req.query;
   if (!url || !selector) res.send("Something wrong with params!");
+  else {
+    try {
+      axios
+        .get(url)
+        .then((response) => {
+          const $ = cheerio.load(response.data);
+          const info = [];
+
+          $(selector).each((index, element) => {
+            info.push(
+              $(element)
+                .text()
+                .replace(/\n|\s{2,}/g, "")
+                .trim()
+            );
+          });
+          console.log(info);
+          res.send(info);
+        })
+        .catch((error) => {
+          console.error(`Error occurred: ${error}`);
+        });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    }
+  }
+});
+
+app.post("/scrapev2", async (req, res) => {
+  console.log(req.body);
+  const { url, selector } = req.body;
+  if (!url || !selector) res.send("Something wrong with body!");
   else {
     try {
       axios
