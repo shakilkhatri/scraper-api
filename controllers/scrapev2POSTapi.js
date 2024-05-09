c891a3f6f7288c279ab9f8b0968679e9065b5dc3
Shakil khatri <khatri.shakilamd@gmail.com>
2024-05-09 06:07:38 +0530

    error handling in post api

--- controllers/scrapev2POSTapi.js
+++ controllers/scrapev2POSTapi.js
@@ -6,18 +6,27 @@ export default async (req, res) => {
   if (!url || !payload) res.send("Something wrong with body!");
   else {
     try {
-      axios
-        .post(url, payload)
-        .then((response) => {
-          console.log(response.data.data);
-          res.send(response.data.data);
-        })
-        .catch((error) => {
-          console.error(`Error occurred: ${error}`);
-        });
+      const response = await axios.post(url, payload);
+      console.log(response.data.data);
+      res.status(200).send(response.data.data);
     } catch (error) {
-      console.error(error);
-      res.status(500).send("An error occurred");
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error("Error response status:", error.response.status);
+        console.error("Error response data:", error.response.data);
+        res
+          .status(200)
+          .send({ status: error.response.status, error: error.response.data });
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("No response received:", error.request);
+        res.status(500).send("No response received from server.");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error setting up request:", error.message);
+        res.status(500).send("Error setting up request: " + error.message);
+      }
     }
   }
 };
